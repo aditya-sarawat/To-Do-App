126ba93eea7e9ddb64b3427ac0efd0c7a4249151
Aditya Sarawat <54805791+aditya-sarawat@users.noreply.github.com>
2021-05-24 10:53:33 +0530

    version 1.25.05

--- assets/alert.js
+++ assets/alert.js
@@ -1,8 +1,7 @@
 const content = document.querySelector(".content");
-
+const input = document.querySelector(".inputvalue");
 
 function addNewElement() {
-	const input = document.querySelector(".inputvalue");
 	if (input.value != "") {
 		const div = document.createElement("div");
 		div.classList.add("todocontent");
@@ -21,7 +20,9 @@ function addNewElement() {
 		div.appendChild(checkButton);
 		div.appendChild(trashButton);
 		content.appendChild(div);
+		addTodo(input.value);
 		input.value = "";
+		console.log(input.value);
 	}
 	else {
 		window.alert("Please Write Something to add!");
@@ -37,4 +38,67 @@ function deletetodo() {
 	this.parentNode.addEventListener("animationend", function() {
 		this.remove();
 	});
+	removeTodo(this.parentNode);
 }
+
+function addTodo(todo) {
+	let todos;
+	if(localStorage.getItem("todos") === null) {
+		todos = [];
+	}
+	else {
+		todos = JSON.parse(localStorage.getItem("todos"));
+	}
+	todos.push(todo);
+	console.log(todo);
+	localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function setCompleted(todo) {
+	
+}
+
+function removeTodo(todo) {
+	let todos;
+	if(localStorage.getItem("todos") === null) {
+		todos = [];
+	}
+	else {
+		todos = JSON.parse(localStorage.getItem("todos"));
+	}
+	const todoIndex = todo.children[0].innerText;
+  	todos.splice(todos.indexOf(todoIndex), 1);
+  	localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function getTodos() {
+	let todos;
+	if (localStorage.getItem("todos") === null) {
+		todos = [];
+	}
+	else {
+		todos = JSON.parse(localStorage.getItem("todos"));
+	}
+	todos.forEach(function(todo) {
+		const div = document.createElement("div");
+		div.classList.add("todocontent");
+		const divcontent = document.createElement("div");
+		divcontent.classList.add("name");
+		divcontent.innerText = todo;
+		console.log(todo);
+		const checkButton = document.createElement("button");
+		checkButton.classList.add("buttonmarginone");
+		checkButton.innerHTML = `<i class="fas fa-check"></i>`;
+		checkButton.addEventListener("click", completed);
+		const trashButton = document.createElement("button");
+		trashButton.classList.add("buttonmargin");
+		trashButton.innerHTML = `<i class="fas fa-trash"></i>`;
+		trashButton.addEventListener("click", deletetodo);
+		div.appendChild(divcontent);
+		div.appendChild(checkButton);
+		div.appendChild(trashButton);
+		content.appendChild(div);
+	});
+}
+
+getTodos();
